0ca5355b276fee0c24e0d3fc7325c26722983246
aarti773 <aarti772005@gmail.com>
2026-05-28 11:43:26 +0530

    Set up Express server and MongoDB connection

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "ClubLens API running",
+  });
+});
+
+module.exports = app;
