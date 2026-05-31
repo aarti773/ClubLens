9a8b0dcff115175a959afe12283647665e4c9b45
aarti773 <aarti772005@gmail.com>
2026-05-31 14:20:13 +0530

    Connect database in Vercel serverless handler

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,15 @@
 const app = require("../src/app");
+const connectDB = require("../src/config/db");
 
-module.exports = app;
+let isConnected = false;
+
+async function handler(req, res) {
+  if (!isConnected) {
+    await connectDB();
+    isConnected = true;
+  }
+
+  return app(req, res);
+}
+
+module.exports = handler;
